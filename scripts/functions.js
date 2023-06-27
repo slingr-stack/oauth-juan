b245b019f729fc2c8a81aad2940e8c2abf8dbba8
juan <jguardiolagarcia@gmail.com>
2023-06-27 14:08:49 -0300

    store in callback

--- scripts/functions.js
+++ scripts/functions.js
@@ -88,6 +88,8 @@ exports.testFunction= function () {
                 sys.logs.error('userConnected callback (test)');
                 sys.logs.error('originalMessage (test)'+ JSON.stringify(originalMessage));
                 sys.logs.error('callbackData (test)'+ JSON.stringify(callbackData));
+                sys.storage.put(callbackData.id +' - refresh_token', response.refresh_token);
+
             }
         }
     });
